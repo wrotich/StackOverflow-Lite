5d97e20df186538c6d33e2aeb9d7044daf27d88e
winniejerop <winniejerop@github.com>
2018-08-17 20:13:06 +0300

    [#159744310] setup api endpoint for fetching all questions

--- app/api.py
+++ app/api.py
@@ -15,6 +15,24 @@ def create_app():
         response = jsonify(data['questions'])
         response.status_code = 200
         return response
-        
+    
+    @app.route('/api/v1/questions/<questionid>', methods=['GET'])
+    def question(id, **kwargs):
+        """This function fetches a single question using a unique id"""
+        questions = data['questions']
+        new_question = {}
+        for q in questions:
+            if int(q['id']) == int(id):
+                new_question = q 
+            
+        if len(new_question) == 0:
+            # question not found
+            # return error 404
+            abort(404)
+        else:
+            # return the question
+            response.status_code = 200
+            return jsonify(new_question)
+
     
     return app
