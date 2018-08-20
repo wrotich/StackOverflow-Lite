8bfabcef7ee8feb24c5de48b182181857bca4b10
winniejerop <winniecherop@outlook.com>
2018-08-20 21:22:57 +0300

    Feedback Implementation

--- app/models.py
+++ app/models.py
@@ -1,29 +1,29 @@
-# Model class for the question
+# Model class for the question part
 class Question:
     def __init__(self, qstn_id, question):
         self.qstn_id = qstn_id
         self.question = question
 
-    # method to enable us acess class attributes as items 
+    # method for acessing class attributes as items 
     def __getitem__(self,item):
         return getattr(self, item) 
     
-    # method to enable us display class objects as dictionaries 
+    # method for displaying class objects as dictionaries 
     def __repr__(self):
         return repr(self.__dict__)
         
 
-# Model class for the answer
+# Model class for the answers part
 class Answer:
     def __init__(self, ans_id, answer, qstn_id):
         self.ans_id = ans_id
         self.answer = answer
         self.qstn_id = qstn_id
 
-    # method to enable us acess class attributes as items 
+    # method for acessing class attributes as items 
     def __getitem__(self,item):
         return getattr(self, item) 
     
-    # method to enable us display class objects as dictionaries 
+    # method for displaying class objects as dictionaries 
     def __repr__(self):
         return repr(self.__dict__)
