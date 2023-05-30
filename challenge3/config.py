be8b6d5ca6acf3d8a22a595fe8b6a40a6c7dd2c2
Winnie Rotich <winniecherop@outlook.com>
2023-05-30 10:25:35 +0300

    Ch #159959551 challenge3 testcases (#19)
    * ch [#159939226] database setup and directory setup
    
    * ch [#159959551] added challenge3 test cases
    
    * Added environment configs
    
    * added the database configs and api models
    
    * [ch-159959551] renamed challenge 3 testcases folder:Feedback Implementation
    
    * Update config.py

---
+++ challenge3/config.py
@@ -0,0 +1,26 @@
+import os
+postgres_local_base = 'postgresql://postgres:@localhost/'
+database_name = 'testdb'
+
+class BaseConfig:
+    """Base configuration."""
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_pin')
+    DEBUG = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration."""
+    DEBUG = True
+   
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration."""
+    DEBUG = True
+    TESTING = True
+  
+
+class ProductionConfig(BaseConfig):
+    """Production configuration."""
+    SECRET_KEY = 'my_pin'
+  
